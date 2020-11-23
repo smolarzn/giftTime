6abe2c83203015292028bc6988c423eeada3216e
smolarzn <04650nat@gmail.com>
2020-11-23 11:27:18 +0100

    first commit

---
+++ src/main/java/com/telego/giftTime/GiftTimeApplication.java
@@ -0,0 +1,13 @@
+package com.telego.giftTime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftTimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/telego/giftTime/GiftTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telego.giftTime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
